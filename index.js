08a26b50b064a2f464f64f20c123f51af1ee51b7
小朱同学 <zx15727547121@163.com>
2022-03-13 16:25:13 +0800

    update

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const axios = require("axios")
 
-;(async () => {
-  const res = await axios.get("https://www.baidu.com/")
-  const data = res.data
-  console.log(data)
-})()
+// ;(async () => {
+//   const res = await axios.get("https://www.baidu.com/")
+//   const data = res.data
+//   console.log(data)
+// })()
+
+console.log("${{ secrets.MY_SECRET }}")
