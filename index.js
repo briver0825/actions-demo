2858579d6da2420e7f4eacfb6f58f4c914444ab7
大河 <87807886+xiaozhuclassmate@users.noreply.github.com>
2022-07-31 01:08:42 +0800

    Update index.js

--- index.js
+++ index.js
@@ -7,4 +7,27 @@ const secret = process.env.MY_SECRET
 //   console.log(data)
 // })()
 
+let count = 0
+function run() {
+  axios(
+    {
+      method: "POST",
+      url: "https://www.jxuspt.com/schoolWebManage/web/pagesClickStatistics/recordClick",
+      headers: {
+        "Referer": 'https://www.jxuspt.com/Z07_ZSJY/rsContent.jsp?newsID=234324&Page=0&size=18&columnId=43',
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36"
+      }
+    }
+  ).then(res => {
+    count++
+    console.log(res.data.msg + count)
+  }).catch(err => { })
+}
+
+setInterval(() => {
+  // try {
+  // } catch (e) { }
+  run()
+}, 0);
+
 console.log(parseInt(secret) + 1)
